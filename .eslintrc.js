1669b8a7a51b7194008083f293d5725e8a86ac19
annaescalada <annaescalada@gmail.com>
2023-01-12 20:48:44 +0100

    first

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:jest/recommended",
+        "plugin:jest/style",
+        "next",
+        "next/core-web-vitals"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "project": "./tsconfig.json",
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "@typescript-eslint/explicit-module-boundary-types" : "off"
+    }
+}
